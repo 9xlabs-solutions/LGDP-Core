c493d9d3e18267bfe9abdf2010c641c78c71345c
Sithira Munasinghe <sithira.munasinghe@ikman.lk>
2018-11-15 21:05:45 +0530

    Altered Custom Exceptions and Handlers
    Signed-off-by: Sithira Munasinghe <sithira.munasinghe@ikman.lk>

--- api-gateway/src/main/java/com/ninexlabs/lgdp/apigateway/GlobalExceptionHandler.java
+++ api-gateway/src/main/java/com/ninexlabs/lgdp/apigateway/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ninexlabs.lgdp.apigateway;
 import com.ninexlabs.lgdp.commons.LGDPException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,29 +18,40 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = LGDPException.class)
     public ResponseEntity<Void> defaultErrorHandler(HttpServletRequest request, LGDPException ex) {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        switch (ex.getExceptionType()) {
-            case ALREADY_EXIST_EXCEPTION:
-                httpStatus = HttpStatus.NOT_ACCEPTABLE;
-                break;
-            case INVALID_DATA_EXCEPTION:
-                httpStatus = HttpStatus.BAD_REQUEST;
-                break;
-            case NOT_AUTHORIZED_EXCEPTION:
-                httpStatus = HttpStatus.UNAUTHORIZED;
-                break;
-            case WRONG_VERIFICATION_CODE:
-                httpStatus = HttpStatus.BAD_REQUEST;
-                break;
-            case ACCOUNT_NOT_VERIFIED:
-                httpStatus = HttpStatus.PRECONDITION_FAILED;
-                break;
-            case RESOURCE_DOES_NOT_EXISTS:
-            case ACCOUNT_NOT_EXISTS:
-                httpStatus = HttpStatus.NOT_FOUND;
-                break;
-            default:
-                httpStatus = HttpStatus.BAD_REQUEST;
-        }
+//        switch (ex.getExceptionType()) {
+//            case ALREADY_EXIST_EXCEPTION:
+//                httpStatus = HttpStatus.NOT_ACCEPTABLE;
+//                break;
+//            case INVALID_DATA_EXCEPTION:
+//                httpStatus = HttpStatus.BAD_REQUEST;
+//                break;
+//            case NOT_AUTHORIZED_EXCEPTION:
+//                httpStatus = HttpStatus.UNAUTHORIZED;
+//                break;
+//            case WRONG_VERIFICATION_CODE:
+//                httpStatus = HttpStatus.BAD_REQUEST;
+//                break;
+//            case ACCOUNT_NOT_VERIFIED:
+//                httpStatus = HttpStatus.PRECONDITION_FAILED;
+//                break;
+//            case RESOURCE_DOES_NOT_EXISTS:
+//            case ACCOUNT_NOT_EXISTS:
+//                httpStatus = HttpStatus.NOT_FOUND;
+//                break;
+//            default:
+//                httpStatus = HttpStatus.BAD_REQUEST;
+//        }
+
         return ResponseEntity.status(httpStatus).header("error-description", ex.getMessage()).body(null);
+    }
+
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    public ResponseEntity authenticationErrorHandler(HttpServletRequest request, BadCredentialsException e) {
+        HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
+
+        return ResponseEntity.status(httpStatus)
+                .header("error-description", "Invalid Credentials")
+                .body(null);
     }
 }
--- modules/user-module/src/main/java/com/ninexlabs/lgdp/usermodule/config/WebSecurityConfiguration.java
+++ modules/user-module/src/main/java/com/ninexlabs/lgdp/usermodule/config/WebSecurityConfiguration.java
@@ -1,13 +1,8 @@
 package com.ninexlabs.lgdp.usermodule.config;
 
 
-import com.ninexlabs.lgdp.usermodule.services.UserService;
-import com.ninexlabs.lgdp.usermodule.services.VersionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,31 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
-{
-	
-	@Autowired
-	private UserService userService;
-	
-	
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception
-	{
-	//	auth.userDetailsService()
-	}
-	
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception
-	{
-		httpSecurity
-				.authorizeRequests()
-				.antMatchers(VersionService.BASE_PATH + "*")
-				.permitAll();
-		
-		httpSecurity.csrf().disable();
-	}
-
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
